5c8ec76d55c603981d82abf2365f28035695ec71
Tijs <TijsRuigrok15@gmail.com>
2021-10-15 14:27:35 +0200

    Auto animations refactored

--- js/instant.js
+++ js/instant.js
@@ -1,27 +1,67 @@
-const landscapeContainer = document.querySelector("#js--landscape-container");
-const landscapeImgFront = document.querySelector("#js--landscape-img-front");
-const landscapeImgMiddle = document.querySelector("#js--landscape-img-middle");
-const landscapeImgBack = document.querySelector("#js--landscape-img-back");
+class Animation {
+    get animationFunction() { return this._animationFunction; }
+    get startState() { return this._startState; }
+    get endState() { return this._endState; }
+    get easingType() { return this._easingType; }
+    get durationMs() { return this._durationMS; }
+    get ticks() { return this._ticks; }
 
-window.onload = function () {
-    animation(scale, landscapeImgFront, 1, 1.2, 4000);
-    // animation(opacity, landscapeImgFront, 0, 1, 4000);
-    // animation(position, landscapeImgFront, [10, 10], [40, 40], 4000);
+    constructor(animationFunction, startState, endState, easingType, durationMS, ticks) {
+        this._animationFunction = animationFunction;
+        this._startState = startState;
+        this._endState = endState;
+        this._easingType = easingType;
+        this._durationMS = durationMS;
+        this._ticks = ticks;
+    }
 }
 
-function animation (animationFunction, targetElement, startValue, endValue, durationMs, ticks = 200) {
-    let value = startValue;
-    animationFunction(targetElement, value);    // TODO: check if right kind of function
+const landscapeContainer = document.querySelector("#js--landscape-container"),
+    landscapeImgFront = document.querySelector("#js--landscape-img-front"),
+    landscapeImgMiddle = document.querySelector("#js--landscape-img-middle"),
+    landscapeImgBack = document.querySelector("#js--landscape-img-back");
 
-    const delay =  durationMs / ticks;
+const sunsetContainer = document.querySelector("#js--sunset-container"),
+    sunsetImgBack = document.querySelector("#js--sunset-img-back"),
+    sunsetImgSun = document.querySelector("#js--sunset-img-sun");
+
+window.onload = function () {
+    const animation1 = new Animation(scale, 1, 1.3, "Linear", 4000, 200);
+    initAnimation(animation1, landscapeImgFront, landscapeContainer);
+}
+
+function initAnimation (animation, targetElement, targetContainer) {
+    let containerReached = false;
+
+    animation.animationFunction(targetElement, animation.startState);
+
+    window.addEventListener("scroll", function () {
+
+        if (window.scrollY >= getTopPos(targetContainer) && !containerReached) {
+            animate(animation, targetElement);
+            containerReached = true;
+        }
+    });
+}
+
+function getTopPos(element) {
+    let rect = element.getBoundingClientRect(),
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return rect.top + scrollTop;
+}
+
+function animate (animation, targetElement) {
+
+    const delay = animation.durationMs / animation.ticks;
     let timePassed = 0;
+    let state = animation.startState;
 
     const interval = setInterval(function () {
         timePassed += delay;
-        if (value >= endValue) clearInterval(interval);
+        if (state >= animation.endState) clearInterval(interval);
 
-        value = startValue + (endValue - startValue) * timePassed / durationMs;
-        animationFunction(targetElement, value);
+        state = easing(animation.easingType, animation.startState, animation.endState, timePassed, animation.durationMs);
+        animation.animationFunction(targetElement, state);
     }, delay);
 }
 
@@ -36,4 +76,18 @@ function opacity (targetElement, opacity) {
 function position (targetElement, position) {
     targetElement.style.left = position[0];
     targetElement.style.top = position[1];
+}
+
+function easing (type, startState, endState, timePassed, duration) {
+
+    switch (type) {
+        case "Linear":
+            return startState + (endState - startState) * timePassed / duration;
+
+        case "Quadratic":
+            return startState + (endState - startState) * Math.pow(timePassed / duration, 2);
+
+        default:
+            return startState + (endState - startState) * timePassed / duration; // Linear
+    }
 }
